2f0d492a8eb5d101dadf5a359d5a5c48e845a075
Lumi Xu <102558017+Lumi-works@users.noreply.github.com>
2023-08-10 16:48:33 -0500

    clean up

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { usePathname, useRouter } from "next/navigation";
+import { useOrganization } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -33,6 +34,7 @@ import { createThread } from "@/lib/actions/thread.actions";
 function PostThread({ userId }: { userId: string }) {
   const router = useRouter();
   const pathname = usePathname();
+  const { organization } = useOrganization();
 
   const form = useForm({
     resolver: zodResolver(ThreadValidation),
@@ -43,10 +45,11 @@ function PostThread({ userId }: { userId: string }) {
   });
 
   const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+    console.log("ORG ID: ", organization);
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
 
